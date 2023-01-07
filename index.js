abee7e7df855e2631fe1dfb0dea6f444b70ab8f8
DannyK <olubanjok@gmail.com>
2023-01-07 07:55:03 +0100

    Configure JavaScript linter; ESLint

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var menu = document.getElementById('menu-items');
-var nav = document.getElementById('navBar')
-var navX = document.getElementById('navX')
+const menu = document.getElementById('menu-items');
+const nav = document.getElementById('navBar');
+const navX = document.getElementById('navX');
 
 function toggleMenu() {
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none'
-        navX.style.display = 'none'
-        nav.style.display = 'flex'
-    }
-    
-    else {
-        menu.style.display = 'flex'
-        navX.style.display = 'block'
-        nav.style.display = 'none'
-    }
+  if (menu.style.display === 'flex') {
+    menu.style.display = 'none';
+    navX.style.display = 'none';
+    nav.style.display = 'flex';
+  } else {
+    menu.style.display = 'flex';
+    navX.style.display = 'block';
+    nav.style.display = 'none';
+  }
 }
+
+toggleMenu();
