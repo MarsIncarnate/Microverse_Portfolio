20f6d48b764382ed88d37bfd0504a2530b24fc42
DannyK <olubanjok@gmail.com>
2023-01-07 20:25:57 +0100

    Add code review requirements: replace static html with dynamic js, remove overflow, blur background

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const detailImg = document.getElementById('detailImg');
 const detailH1 = document.getElementById('detailH1');
 const detailUl = document.getElementById('detailUl');
 const detailP = document.getElementById('detailP');
+const detailCont = document.getElementById('detailCont');
+const portfolio = document.getElementById('portfolio');
 const Details = [
   {
     img: 'images/details-image.png',
     h1: 'Keeping track of hundreds of components',
+    h3: 'Multi-Post Stories<br>Gain+Glory',
+    ul3: ['Ruby on rails', 'css', 'JavaScript', 'html'],
     ul: ['Ruby on rails', 'css', 'JavaScript'],
     ul2: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal'],
     p: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
@@ -17,6 +21,8 @@ const Details = [
   {
     img: 'images/details-image1.png',
     h1: 'Keeping track of hundreds of components',
+    h3: 'Multi-Post Stories<br>Gain+Glory',
+    ul3: ['Ruby on rails', 'css', 'JavaScript', 'html'],
     ul: ['Ruby on rails', 'css', 'JavaScript'],
     ul2: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal'],
     p: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
@@ -24,6 +30,8 @@ const Details = [
   {
     img: 'images/details-image2.png',
     h1: 'Keeping track of hundreds of components',
+    h3: 'Multi-Post Stories<br>Gain+Glory',
+    ul3: ['Ruby on rails', 'css', 'JavaScript', 'html'],
     ul: ['Ruby on rails', 'css', 'JavaScript'],
     ul2: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal'],
     p: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
@@ -31,6 +39,8 @@ const Details = [
   {
     img: 'images/details-image.png',
     h1: 'Keeping track of hundreds of components',
+    h3: 'Multi-Post Stories<br>Gain+Glory',
+    ul3: ['Ruby on rails', 'css', 'JavaScript', 'html'],
     ul: ['Ruby on rails', 'css', 'JavaScript'],
     ul2: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal'],
     p: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
@@ -38,6 +48,8 @@ const Details = [
   {
     img: 'images/details-image1.png',
     h1: 'Keeping track of hundreds of components',
+    h3: 'Multi-Post Stories<br>Gain+Glory',
+    ul3: ['Ruby on rails', 'css', 'JavaScript', 'html'],
     ul: ['Ruby on rails', 'css', 'JavaScript'],
     ul2: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal'],
     p: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
@@ -45,11 +57,20 @@ const Details = [
   {
     img: 'images/details-image2.png',
     h1: 'Keeping track of hundreds of components',
+    h3: 'Multi-Post Stories<br>Gain+Glory',
+    ul3: ['Ruby on rails', 'css', 'JavaScript', 'html'],
     ul: ['Ruby on rails', 'css', 'JavaScript'],
     ul2: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal'],
     p: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   },
 ];
+
+for (let i = 0; i < Details.length; i += 1) {
+  const item = portfolio.getElementsByClassName('portfolio-item')[i];
+  item.getElementsByTagName('img')[0].src = Details[i].img;
+  item.getElementsByTagName('ul')[0].innerHTML = Details[i].ul3.map((item) => `<li>${item}</li>`).join('');
+  item.getElementsByTagName('h3')[0].innerHTML = Details[i].h3;
+}
 
 function toggleMenu() {
   if (menu.style.display === 'flex') {
@@ -65,6 +86,7 @@ function toggleMenu() {
 
 function revealDetails(a) {
   detail.style.display = 'flex';
+  detailCont.style.display = 'block';
   if (window.screen.width < 768) {
     detail.style.top = `${(748 + 474 * a)}px`;
     detailUl.innerHTML = Details[a].ul.map((item) => `<li>${item}</li>`).join('');
@@ -72,7 +94,6 @@ function revealDetails(a) {
     detail.style.top = '1159px';
     detailUl.innerHTML = Details[a].ul2.map((item) => `<li>${item}</li>`).join('');
   }
-  document.getElementById('root').style.backgroundColor = '#c1c7d0';
   detailImg.src = Details[a].img;
   detailH1.innerHTML = Details[a].h1;
   detailP.innerHTML = Details[a].p;
@@ -80,7 +101,7 @@ function revealDetails(a) {
 
 function closeDetails() {
   detail.style.display = 'none';
-  document.getElementById('root').style.backgroundColor = 'unset';
+  detailCont.style.display = 'none';
 }
 
 if (nav === '20') {
